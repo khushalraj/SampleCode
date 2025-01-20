b564361c9041e6f7bfc05d975a72f2fae4179ac6
khushalraj <62936239+khushalraj@users.noreply.github.com>
2025-01-20 17:45:52 +0530

    Update GitHubSampleCode.java

--- src/Demo/GitHubSampleCode.java
+++ src/Demo/GitHubSampleCode.java
@@ -3,5 +3,8 @@ package Demo;
 public class GitHubSampleCode {
 public static void main(String[] args) {
 	System.out.println("Wleomce to gihub account!!");
+	System.out.println("add some code here");
+	System.out.println("Java Developer");
+  
 }
 }
